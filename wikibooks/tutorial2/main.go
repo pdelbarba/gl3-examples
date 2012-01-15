d435c7f22483f3f85c093601bde47352f5660c25
Marcel Hauf <marcel.hauf@googlemail.com>
2012-01-15 23:13:18 +0100

    tutorial2: converted to new OpenGL binding

--- wikibooks/tutorial2/main.go
+++ wikibooks/tutorial2/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"
 
-	"gl"
+	gl "github.com/chsc/gogl/gl33"
 	"github.com/jteeuwen/glfw"
 )
 
@@ -25,14 +25,14 @@ var triangleVertices = []float32{
 	-0.8, -0.8,
 	0.8, -0.8,
 }
-var vboTriangle gl.Buffer
+var vboTriangle gl.Uint
 
-var vs gl.Shader
-var fs gl.Shader
-var program gl.Program
-var attributeCoord2d gl.AttribLocation
+var vs gl.Uint
+var fs gl.Uint
+var program gl.Uint
+var attributeCoord2d gl.Uint
 
-func fileRead(name string) (string, os.Error) {
+func fileRead(name string) (string, error) {
 	b, err := ioutil.ReadFile(name)
 	if err != nil {
 		return "", err
@@ -40,41 +40,42 @@ func fileRead(name string) (string, os.Error) {
 	return string(b), nil
 }
 
-func createShader(name string, shaderType int) (gl.Shader, os.Error) {
+func createShader(name string, shaderType int) (gl.Uint, error) {
 	data, err := fileRead(name)
 	if err != nil {
 		return 0, err
 	}
 	if len(data) == 0 {
-		return 0, os.NewError("No shader code.")
+		return 0, errors.New("No shader code.")
 	}
-	var shader gl.Shader
+	var shader gl.Uint
 	switch shaderType {
 	case VertexShaderType:
 		shader = gl.CreateShader(gl.VERTEX_SHADER)
 	case FragmentShaderType:
 		shader = gl.CreateShader(gl.FRAGMENT_SHADER)
 	default:
-		return 0, os.NewError("Unknown ShaderType.")
+		return 0, errors.New("Unknown ShaderType.")
 	}
-	shader.Source(data)
-	shader.Compile()
+	src := gl.GLStringArray(string(data))
+	defer gl.GLStringArrayFree(src)
+	gl.ShaderSource(shader, gl.Sizei(1), &src[0], nil)
+	gl.CompileShader(shader)
 
 	// Similar to print_log in the C code example
-	infoLog := shader.GetInfoLog()
-	if len(infoLog) != 0 {
-		shader.Delete()
-		return 0, err
-	}
-	errNum := gl.GetError()
-	if errNum != 0 {
-		return 0, os.NewError(fmt.Sprintf("Error code: %d", errNum))
+	var length gl.Int
+	gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &length)
+	if length > 1 {
+		glString := gl.GLStringAlloc(gl.Sizei(length))
+		defer gl.GLStringFree(glString)
+		gl.GetShaderInfoLog(shader, gl.Sizei(length), nil, glString)
+		return 0, errors.New(fmt.Sprintf("Shader log: %s", gl.GoString(glString)))
 	}
 	return shader, nil
 }
 
 func initResources() {
-	var err os.Error
+	var err error
 	// Load shaders
 	vs, err = createShader("triangle.v.glsl", VertexShaderType)
 	if err != nil {
@@ -86,35 +87,36 @@ func initResources() {
 		fmt.Printf("Shader: %s\n", err)
 		return
 	}
-
-	// Create GLSL program with loaded shaders
+	var compileOk gl.Int
 	program = gl.CreateProgram()
-	program.AttachShader(vs)
-	program.AttachShader(fs)
-	program.Link()
-	infoLog := program.GetInfoLog()
-	if len(infoLog) != 0 {
-		fmt.Printf("Program: %s\n", infoLog)
+	gl.AttachShader(program, vs)
+	gl.AttachShader(program, fs)
+	gl.LinkProgram(program)
+	gl.GetProgramiv(program, gl.LINK_STATUS, &compileOk)
+	if compileOk == 0 {
+		fmt.Printf("Error in program.\n")
 	}
 
 	// Get the attribute location from the GLSL program (here from the vertex shader)
-	attributeName := "coord2d"
-	attributeCoord2d = program.GetAttribLocation(attributeName)
-	if attributeCoord2d == -1 {
-		fmt.Printf("Could not bind attribute %s\n", attributeName)
+	attributeName := gl.GLString("coord2d")
+	defer gl.GLStringFree(attributeName)
+	attributeTemp := gl.GetAttribLocation(program, attributeName)
+	if attributeTemp == -1 {
+		fmt.Printf("Could not bind attribute %s\n", gl.GoString(attributeName))
 	}
+	attributeCoord2d = gl.Uint(attributeTemp)
 
 	// Generate a buffer for the VertexBufferObject
-	vboTriangle = gl.GenBuffer()
-	vboTriangle.Bind(gl.ARRAY_BUFFER)
+	gl.GenBuffers(1, &vboTriangle)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vboTriangle)
 	// Submit the vertices of the triangle to the graphic card
-	gl.BufferData(gl.ARRAY_BUFFER, len(triangleVertices)*4, triangleVertices, gl.STATIC_DRAW)
-	// Unset the active buffer
-	gl.Buffer(0).Bind(gl.ARRAY_BUFFER)
+	gl.BufferData(gl.ARRAY_BUFFER, gl.Sizeiptr(len(triangleVertices)*4), gl.Pointer(&triangleVertices[0]), gl.STATIC_DRAW)
+	// Unbind the active buffer
+	gl.BindBuffer(gl.ARRAY_BUFFER, 0)
 }
 
 func main() {
-	var err os.Error
+	var err error
 	err = glfw.Init()
 	if err != nil {
 		fmt.Printf("GLFW: %s\n", err)
@@ -143,9 +145,10 @@ func main() {
 		fmt.Println("You can try to lower the settings in glfw.OpenWindowHint(glfw.OpenGLVersionMajor/Minor.")
 	}
 
-	initStatus := gl.Init() // Init glew
-	if initStatus != 0 {
-		fmt.Printf("Error-code: %d Init-Status: %d\n", gl.GetError(), initStatus)
+	// Init extension loading
+	err = gl.Init()
+	if err != nil {
+		fmt.Printf("Init OpenGL extension loading failed with %s.\n", err)
 	}
 
 	// Enable transparency in OpenGL
@@ -163,8 +166,8 @@ func main() {
 }
 
 func free() {
-	program.Delete()
-	vboTriangle.Delete()
+	gl.DeleteProgram(program)
+	gl.DeleteBuffers(1, &vboTriangle)
 }
 
 func display() {
@@ -173,18 +176,19 @@ func display() {
 	gl.Clear(gl.COLOR_BUFFER_BIT)
 
 	// Use the GLSL program
-	program.Use()
+	gl.UseProgram(program)
 
-	vboTriangle.Bind(gl.ARRAY_BUFFER)
-	attributeCoord2d.EnableArray()
+	gl.BindBuffer(gl.ARRAY_BUFFER, vboTriangle)
+	gl.EnableVertexAttribArray(attributeCoord2d)
 
 	// Describe our vertices array to OpenGL (it can't guess its format automatically)
-	attributeCoord2d.AttribPointerOffset(2, gl.FLOAT, false, 0, 0)
+	gl.VertexAttribPointer(attributeCoord2d, 2, gl.FLOAT, gl.FALSE, 0, gl.Pointer(nil))
 
 	// Push each element in buffer_vertices to the vertex shader
 	gl.DrawArrays(gl.TRIANGLES, 0, 3)
 
-	attributeCoord2d.DisableArray()
+	gl.DisableVertexAttribArray(attributeCoord2d)
+	gl.BindBuffer(gl.ARRAY_BUFFER, 0) // Unbind
 
 	// Display the result
 	glfw.SwapBuffers()
